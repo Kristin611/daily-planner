328480cde6c35c132e68eda64b9559af000dba95
Kris10 <kmrichie611@gmail.com>
2023-09-19 16:50:00 -0500

    added displayTimeBlock function

--- Develop/script.js
+++ Develop/script.js
@@ -15,8 +15,31 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+    function displayTimeBlock () {
+        const currentHour = dayjs().format('H'); //create a variable for current hour to compare be able to compare it to the hour in the div
+
+        for (let hour = 9; hour <= 17; hour++) { //create a loop to iterate through the avaiable hours 9 AM - 17 PM, i.e., 5 PM
+          const timeBlock = $(`#hour-${hour}`) //created a variable to find the corresponding HTML element by id interpolation for example 'hour-9'
+        
+          if (!timeBlock) {
+            continue; //the condition sees if a timeBlock does not exist or is undefined and continues on to the next hour
+          }
+        }   
+
+        const divHour = $('#hour-9'); //grabbing HTML element with id 'hour-9'
+        const timeBlock2 = parseInt(divHour.attr('id').split('-')[1], 10); 
+
+        if (timeBlock2 < currentHour) {
+          timeBlock.classList.add('past');
+        } else if (timeBlock2 === currentHour) {
+          timeBlock.classList.add('present');
+        } else {
+          timeBlock.classList.add('future')
+        }
+    }  
 
     
+
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -28,7 +51,8 @@ $(function () {
       console.log(currentDate)
     $('#currentDay').text(currentDate);
     
-  
 
 
 });
+
+//stopped at adding function and trying to get colors to display. Something needs to change either id needs to be adjusted or 
