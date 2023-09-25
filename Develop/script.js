841be329cdb1caf5ace2b1f065cadcb90820f0ef
Kris10 <kmrichie611@gmail.com>
2023-09-25 13:23:47 -0500

    added comments to HTML and JS files

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -9,34 +10,14 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-  // 1. Use the id to grab the button element to add a click event listener to it using jQuery, but they all have different ids so....
-      //a. to access div#hour-9 using the DOM: document.body.children[1].children[0-8]
-      //ex. const target = document.body.children[1].children[0-8]
-      //ex. const saveBtn9 = document.body.children[1].children[0].children[2] or 
-        
-  // 2. When save button is clicked,  user input is saved in local storage
-
-    //function saveUserInput() {
-    // const textInput = document.body.children[1].children[0].children[1].value // grabbing text area for div#hour-9--should I create a loop so I don't have to do this for each div?
-    // console.log(textInput)
-
-    // const text = localStorage.getItem('text') //storing user text into variable 'text'
-    // textInput.textContent = text // actual user text being stored into text variable
-      //}
-
-    $('.saveBtn').on('click', function() {
-      const textInput = $(this).siblings('.description').val()
+    $('.saveBtn').on('click', function() { //I used jQuery to grab the save button to attach a click event listener to it. Every time the saved button is clicked, the user's text will be saved/set in local storage. 
+      const textInput = $(this).siblings('.description').val() //I created a textInput value to save user's text. 'this' is used to reference the object I am currently in
       const time = $(this).parent().attr('id')
-      console.log(time, textInput)
-      localStorage.setItem(time, textInput)
-    }) //grabbing save button for to create event listener
-
-    
+      //console.log(time, textInput)
+      localStorage.setItem(time, textInput) //time is they key and textInput is the value.
+    }) 
 
   
-        
-  
-
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -44,18 +25,16 @@ $(function () {
   // current hour in 24-hour time?
   //
     function displayTimeBlockColor () {
-        let currentHour = dayjs().hour(); //create a variable for current hour to be able to compare it to the hour in the div
+        let currentHour = dayjs().hour(); //I created a variable for current hour to be able to compare it to the hour in the div for the conditional statements.
         //console.log(currentHour) //this works
 
-        // const divIDs = ['hour-9', 'hour-10', 'hour-11', 'hour-12', 'hour-13', 'hour-14', 'hour-15', 'hour-16', 'hour-17'];
-        //console.log(divIDs) //this works
-
-        $('.time-block').each( function () {
+        $('.time-block').each( function () { //I grabbed the corresponding element by id (using jQuery). I used a for each loop to iterate over the element using the element's id.
           console.log('h')
-          let timeBlockHour = parseInt($(this).attr('id').split('-')[1]); //grabbing the element by id (using jQuery) and looping through each div id in order to compare it to currentHour. i+1 moves i over 1 index 
-          //console.log(timeBlockHour) //I think this is working? .spit splitting over everything before wtvr is in()
+          let timeBlockHour = parseInt($(this).attr('id').split('-')[1]); //
+          //i+1 moves i over 1 index 
+           //.split splitting over everything before wtvr is in()
 
-          if (timeBlockHour < currentHour) {
+          if (timeBlockHour < currentHour) { //using conditional statements to apply the appropriate color scheme depending on whether the timeBlockHour is past, present or future.
             console.log(timeBlockHour, currentHour)
             $(this).addClass('past');
           } else if (timeBlockHour === currentHour) {
@@ -66,7 +45,7 @@ $(function () {
         })
 
     }  
-    displayTimeBlockColor();
+    displayTimeBlockColor(); //calling the function
 
     //setInterval(displayTimeBlockColor, 15000)
 
@@ -76,15 +55,15 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  for (let i = 9; i <= 17; i++) {
-    $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`))
+  for (let i = 9; i <= 17; i++) { // I created a for loop to iterate through time block ids using index i to retreive items saved in local storage.
+    $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`)) //getItem is to retreive the data from local storage
   }
   
   // TODO: Add code to display the current date in the header of the page.
-    //var currentDate = $('#currentDay'); -- how come this wouldn't work?
-    let currentDate = dayjs().format('dddd, MMMM DD, YYYY');
+    
+    let currentDate = dayjs().format('dddd, MMMM DD, YYYY'); //I created a variable to store the current day in using Day.js. 
       //console.log(currentDate)
-    $('#currentDay').text(currentDate);
+    $('#currentDay').text(currentDate); //used jQuery to grab the corresponding <p> element to display current day and time on webpage. 
     
 
 
